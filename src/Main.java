30eef50721d944e1c97e4f96d10e8eb615b808a5
Hugo HOVHANNESSIAN <hugo.hovhannessian@etu.univ-tours.fr>
2023-10-16 14:15:57 +0200

    Refractor : Refractor all the base before doing the parser

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Matiere math = new Matiere();
+        math.setLabelMatiere("Math");
+        System.out.println("L'UE de RO est composé de "+math.getLabelMatiere());
     }
-
-    //
 }
